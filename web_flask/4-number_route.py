008e867926a35fdf1b68dde3622ce69e72e2b8e5
Peter Omollo <omollpeter@gmail.com>
2024-05-16 17:27:59 +0300

    Write a script the starts a flask web application

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -47,5 +47,13 @@ def python_text(text):
     return "Python " + text
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Displays a number from the URL if an integer
+    """
+    return f"{n} is a number"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
